b589c080cf9b2523eb852648011181d3550d8432
= <=>
2024-02-24 12:32:14 -0300

    inicio

---
+++ src/modulo4/Exercicio1.java
@@ -0,0 +1,19 @@
+package modulo4;
+
+import java.util.Scanner;
+
+public class Exercicio1 {	
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int num1,num2;
+		
+		num1 = sc.nextInt();
+		num2 = sc.nextInt();
+		
+		sc.close();
+		System.out.println("SOMA = " + (num1+num2));
+
+	}
+
+}

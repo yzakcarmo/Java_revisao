fbb0921999f6d103e1b6b3adfdee90cec3bb8641
= <=>
2024-02-24 16:40:32 -0300

    README adicionado, fim modulo4

---
+++ README.md
@@ -0,0 +1,3 @@
+# Revisão Java
+
+## Minha revisão de Java usando o curso "Java COMPLETO Programação Orientada a Objetos + Projetos"
---
+++ src/modulo4/Exercicio4.java
@@ -0,0 +1,18 @@
+package modulo4;
+
+import java.util.Scanner;
+
+public class Exercicio4 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int numFunc = scan.nextInt();
+        int quantHoras = scan.nextInt();
+        float valorHora = scan.nextFloat();
+
+        System.out.println("NUMBER = " + numFunc);
+        System.out.printf("SALARY = U$ %.2f", valorHora*quantHoras);
+
+        scan.close();
+    }
+}

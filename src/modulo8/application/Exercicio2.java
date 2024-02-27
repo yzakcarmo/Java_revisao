196745f43765fd05531a608494004b68b09ea183
= <=>
2024-02-26 22:56:28 -0300

    Fiz 2 exercicios do modulo8

---
+++ src/modulo8/application/Exercicio2.java
@@ -0,0 +1,24 @@
+package modulo8.application;
+
+import java.util.Scanner;
+
+import modulo8.entities.Employee;
+
+public class Exercicio2 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String nome = scan.nextLine();
+        double salary = scan.nextDouble();
+        double tax = scan.nextDouble();        
+
+        Employee empregado = new Employee(nome, salary, tax);
+
+        System.out.printf("Empregado: %s, $ %.2f \n",empregado.getName(), empregado.netSalary());
+
+        System.out.print("Qual % de aumento: ");
+        empregado.increaseSalary(scan.nextDouble());
+        scan.close();
+
+        System.out.printf("Atualizado: %s, $ %.2f",empregado.getName(), empregado.netSalary());
+    }
+}

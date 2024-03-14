9e073e0c6911ae2753a8b0b6ed325b8ada80ce78
Yzak Carmo <yzak.carmo@gmail.com>
2024-03-13 21:02:03 -0300

    fim modulo15

--- src/modulo15/exercicio1/model/entities/Reservation.java
+++ src/modulo15/exercicio1/model/entities/Reservation.java
@@ -4,6 +4,8 @@ import java.time.Duration;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
+import modulo15.exercicio1.model.exceptions.DomainException;
+
 public class Reservation {
     public static final DateTimeFormatter fmt = DateTimeFormatter.ofPattern("dd/MM/yyyy");
     private Integer roomNumber;
@@ -13,6 +15,8 @@ public class Reservation {
     public Reservation() {}
 
     public Reservation(Integer roomNumber, LocalDate checkIn, LocalDate checkOut) {
+        if(checkOut.isBefore(LocalDate.now()) || checkIn.isBefore(LocalDate.now()))
+        throw new DomainException("As datas de reserva devem ser futuras");
         this.roomNumber = roomNumber;
         this.checkIn = checkIn;
         this.checkOut = checkOut;
@@ -31,13 +35,31 @@ public class Reservation {
     }
     
     public int duration() {
-        Duration duration = Duration.between(checkOut, checkIn);
+        Duration duration = Duration.between(checkIn.atStartOfDay(),checkOut.atStartOfDay());
         return (int)duration.toDays();
+    }
+
+    public void updateDates(LocalDate checkIn, LocalDate checkOut){
+        if(checkOut.isBefore(LocalDate.now()) || checkIn.isBefore(LocalDate.now()))
+        throw new DomainException("As datas de atualização da reserva devem ser futuras");
+
+        if(checkOut.isBefore(checkIn))
+        throw new DomainException("A data de check-out deve ser depois do check-in");        
+
+        this.checkIn = checkIn;
+        this.checkOut = checkOut;
     }
 
     @Override
     public String toString () {
         StringBuilder out = new StringBuilder();
+        out.append("Reservation: Room ");
+        out.append(roomNumber);
+        out.append(", check-in: ");
+        out.append(fmt.format(checkIn));
+        out.append(", check-out: ");
+        out.append(fmt.format(checkOut));
+        out.append(", " + duration() + " nights");
         return out.toString();
     }
 }
---
+++ src/modulo15/exercicio1/model/exceptions/DomainException.java
@@ -0,0 +1,7 @@
+package modulo15.exercicio1.model.exceptions;
+
+public class DomainException extends RuntimeException {
+    public DomainException (String msg) {
+        super(msg);
+    }
+}

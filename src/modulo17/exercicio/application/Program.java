de532521fe21fbf427670480eb48236ba0c578e5
Yzak Carmo <yzak.carmo@gmail.com>
2024-03-16 19:45:01 -0300

    continuando o exercicio

--- src/modulo17/exercicio/application/Program.java
+++ src/modulo17/exercicio/application/Program.java
@@ -1,21 +1,49 @@
 package modulo17.exercicio.application;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Program {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        List<String> values = new ArrayList<>();
         //String source = sc.nextLine();
+        sc.close();
         String source = "src\\modulo17\\file\\exemplo.csv";
-        try(BufferedReader bf = new BufferedReader(new FileReader(source))) {
-            File path = new File(source);
+
+        try(BufferedReader br = new BufferedReader(new FileReader(source))) {
+            new File(source + "\\out").mkdir();           
+            String line = br.readLine();           
+            while (line != null) {
+                StringBuilder text = new StringBuilder();
+                String[] lines = line.split(",");
+
+                Double summary = Double.valueOf(lines[1]) * Integer.valueOf(lines[2]);                
+                text.append(lines[0] + ",");
+
+                values.add(text.toString());
+                line = br.readLine();
+            }            
         }
         catch(IOException e) {
             e.printStackTrace();
-        }        
+        }
+        
+        try(BufferedWriter bw = new BufferedWriter(new FileWriter(source + "\\out\\sumarry.csv"))) {
+            for(String value : values) {
+                bw.write(value);
+                bw.newLine();
+            }
+        }
+        catch(IOException e) {
+            e.printStackTrace();
+        }
     }    
 }
